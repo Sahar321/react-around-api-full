c4ce74b8efc0b697873bc1ac10062613b7a5baae
Sahar Moshe <saharm123456@gmail.com>
2022-08-24 13:33:58 +0300

    ..

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -9,7 +9,7 @@ const { celebrate, Joi } = require('celebrate');
 
 // USE .ALL - POST/GET SHOULD BE INSIDE
 router.all('/cards*', authorized, cards);
-router.all('/users*', authorized,users);
+router.all('/users*', authorized, users);
 
 router.get('/crash-test', () => {
   setTimeout(() => {
@@ -21,7 +21,7 @@ router.all('/signin', signin);
 router.all('/signup', signup);
 
 router.all('*',(req, res) => {
-  res.status(404).json({ message: 'Requested resource not found!' });
+  res.status(404).json({ message: 'Requested resource not found' });
 });
 
 module.exports = router;
