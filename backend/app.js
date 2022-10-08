fd988abdd1bf724834d15a232af0effa05241bed
Sahar Moshe <saharm123456@gmail.com>
2022-10-09 02:39:53 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
 res.header("Access-Control-Allow-Origin", "*");
+
+
+const { method } = req; // Saving the request type (HTTP method) to the corresponding variable
+
+// Default value for Access-Control-Allow-Methods header (all request types are allowed)
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
+// If this is a preliminary request, add the required headers
+if (method === 'OPTIONS') {
+    // allowing cross-domain requests of any type (default)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+}
+
+// saving the list of headers of the original request
+const requestHeaders = req.headers['access-control-request-headers'];
+if (method === 'OPTIONS') {
+    // allowing cross-domain requests with these headers
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // finish processing the request and return the result to the client
+
+}
+
+
 // app middleware
 app.use(requestLogger);
 
