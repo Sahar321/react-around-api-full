51865dfbfcdffdb971272f9ba81ed210090e53bc
Sahar Moshe <saharm123456@gmail.com>
2023-05-01 19:14:50 +0300

    change api port

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -8,12 +8,12 @@ class Auth {
   customFetch = (url, options) => {
     const token = localStorage.getItem('jwt');
     options.headers.authorization = `Bearer ${token}`;
-
-    return fetch(url, options).then(
-      async (res) => (res.ok ? res.json() : Promise.reject(await res.json()))
-      // wait error message(and not only the status code)
-      // from the server.
-    );
+    return fetch(url, options)
+      .then(
+        async (res) => (res.ok ? res.json() : Promise.reject(await res.json()))
+        // wait error message(and not only the status code)
+        // from the server.
+      )
   };
 
   signin = (data) => {
@@ -41,7 +41,7 @@ class Auth {
 }
 
 const token = localStorage.getItem('jwt');
-const one1 = 'http://localhost:3000';
+const one1 = 'http://localhost:3001';
 const one2 = 'https://api.xvr.students.nomoredomainssbs.ru';
 
 const auth = new Auth({
