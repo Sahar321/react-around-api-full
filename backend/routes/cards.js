d37a23f694b186b3b51c339dd6bde677f90cb740
Sahar Moshe <saharm123456@gmail.com>
2023-05-20 15:33:05 +0300

    improve validation

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,5 +1,5 @@
-const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
+const router = require("express").Router();
+const { celebrate, Joi } = require("celebrate");
 
 const {
   getAllCards,
@@ -7,14 +7,20 @@ const {
   deleteCardById,
   unLike,
   addLike,
-} = require('../controllers/cards');
+} = require("../controllers/cards");
 
 const ValidationSchema = {
   createNewCard: celebrate({
     body: Joi.object()
       .keys({
         name: Joi.string().required().min(2).max(30),
-        link: Joi.string().required().uri(),
+        link: Joi.string()
+          .required()
+          .regex(/\.(jpg|jpeg|png|gif)$/i)
+          .messages({
+            "string.pattern.base":
+              "Please ensure that the URL provided for the image is a valid image URL ending with .jpg, .jpeg, .png, or .gif extensions.",
+          }),
       })
       .unknown(true),
   }),
@@ -27,14 +33,14 @@ const ValidationSchema = {
   }),
 };
 
-router.get('/cards', getAllCards);
+router.get("/cards", getAllCards);
 
-router.post('/cards', ValidationSchema.createNewCard, createNewCard);
+router.post("/cards", ValidationSchema.createNewCard, createNewCard);
 
-router.delete('/cards/:cardId', ValidationSchema.cardId, deleteCardById);
+router.delete("/cards/:cardId", ValidationSchema.cardId, deleteCardById);
 
-router.delete('/cards/:cardId/likes/', ValidationSchema.cardId, unLike);
+router.delete("/cards/:cardId/likes/", ValidationSchema.cardId, unLike);
 
-router.put('/cards/:cardId/likes', addLike);
+router.put("/cards/:cardId/likes", addLike);
 
 module.exports = router;
