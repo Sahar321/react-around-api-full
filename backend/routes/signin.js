5008f8a9343cc167d652cc50a7bc1d0a54d0d887
Sahar Moshe <saharm123456@gmail.com>
2023-05-20 15:33:17 +0300

    improve validation

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -7,9 +7,11 @@ const LoginValidationSchema = celebrate({
   body: Joi.object()
     .keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(6),
+      password: Joi.string().required().min(6).max(30),
     })
-    .unknown(true)
+    .options({
+      abortEarly: false,
+    }),
 });
 
 router.post("/signin", LoginValidationSchema, login);
--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -5,7 +5,7 @@ const { createNewUser } = require('../controllers/users');
 const registerValidationSchema = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
+    password: Joi.string().required().min(6).max(30),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().uri(),
