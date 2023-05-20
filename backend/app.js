71af10f16ea6f057ce13ebf0e3e57db4f3262519
Sahar Moshe <saharm123456@gmail.com>
2023-05-20 15:32:47 +0300

    improve validation error massage

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-const cors = require('cors');
-const express = require('express');
-const { errors, isCelebrateError } = require('celebrate');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const router = require('./routes/router');
-const TooManyRequestsError = require('./middleware/errors/TooManyRequestsError');
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const { errors, isCelebrateError } = require("celebrate");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const router = require("./routes/router");
+const TooManyRequestsError = require("./middleware/errors/TooManyRequestsError");
 
 // Apply the rate limiting middleware to all requests
 
@@ -21,21 +21,22 @@ const limiter = rateLimit({
     throw new TooManyRequestsError();
   },
 });
-const { requestLogger, errorLogger } = require('./middleware/logger');
+const { requestLogger, errorLogger } = require("./middleware/logger");
 // settings
 const { PORT = 3001 } = process.env;
 const handleMainError = (err, req, res, next) => {
-  // if an error has no status, display 500
-  let { statusCode = 500 } = err;
-  const { message } = err;
-  // change status code if is CelebrateError, otherwise
-  // it will send the default error message
   if (isCelebrateError(err)) {
-    statusCode = 400;
+    const errorMessage = err.details.get("body");
+    const errorBuilder = errorMessage.details.map(({ message }, index) => {
+      const path = errorMessage.details[index].path[0];
+      const result = message.replace(`"${path}"`, `${path}: `);
+      return `${result} \n`;
+    });
+    return res.status(500).send({ message: errorBuilder });
   }
-
+  const { statusCode = 500, message } = err;
   res.status(statusCode).send({
-    message: statusCode === 500 ? 'An error occurred on the server' : message,
+    message: statusCode === 500 ? "An error occurred on the server" : message,
   });
 };
 
@@ -45,7 +46,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(limiter);
 // app middleware
 app.use(errors());
@@ -56,7 +57,7 @@ app.use(errorLogger);
 app.use(handleMainError);
 
 // database
-mongoose.connect('mongodb://localhost:27017/aroundb');
+mongoose.connect("mongodb://localhost:27017/aroundb");
 
 // Server
 app.listen(PORT);
